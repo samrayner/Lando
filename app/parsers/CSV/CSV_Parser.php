4131ebabc193db3b2db5fb6257159ad6d330b5be
Sam Rayner <samrayner@gmail.com>
2011-10-20 17:05:40 +0100

    Improve CSV parser

--- app/parsers/CSV/CSV_Parser.php
+++ app/parsers/CSV/CSV_Parser.php
@@ -4,12 +4,12 @@ class CSV_Parser {
 	public function parse($str) {
 		$html = "<table>\n\t<thead>\n";
 		
-		foreach(explode("\n", $str) as $i => $row) {		
+		foreach(str_getcsv($str, "\n") as $i => $row) {			
 			$html .= "\t\t<tr>";
 			
 			$cell_tag = ($i == 0) ? "th" : "td";
 			
-			foreach(explode(",", $row) as $cell)
+			foreach(str_getcsv($row, ",") as $cell)
 				$html .= "\n\t\t\t<$cell_tag>".htmlspecialchars($cell)."</$cell_tag>";
 
 			$html .= "\n\t\t</tr>\n";
