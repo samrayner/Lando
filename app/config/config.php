9ef05d101904628eccbcb119f7b272cd59e2656f
Sam Rayner <samrayner@gmail.com>
2011-12-09 14:38:46 +0000

    Fix install checking for config

---
+++ app/config/config.php
@@ -0,0 +1,43 @@
+<?php
+
+$config = array (
+  'admin_password' => 'spiral',
+  'site_title' => 'Bespin Daily',
+  'site_description' => 'All the latest from the cloud city.',
+  'site_root' => 'http://lando.dev',
+  'pretty_urls' => '1',
+  'cache_on_load' => 0,
+  'host' => 'dropbox',
+  'host_root' => '/Lando',
+  'theme' => 'default',
+  'smartypants' => '1',
+  'page_order' => 
+  array (
+    'home' => 
+    array (
+    ),
+    'about-me' => 
+    array (
+    ),
+    'contact' => 
+    array (
+      'everyone' => 
+      array (
+      ),
+      'just-the-ceo' => 
+      array (
+        '_hidden' => true,
+      ),
+      'just-me' => 
+      array (
+        'by-pigeon' => 
+        array (
+          '_hidden' => true,
+        ),
+        'by-email' => 
+        array (
+        ),
+      ),
+    ),
+  ),
+);
--- install/install.php
+++ install/install.php
@@ -1,6 +1,9 @@
 <?php
-$doc_root = dirname(dirname(dirname(__FILE__)));
-include "$doc_root/app/core/loader.php";
+$doc_root = dirname(dirname(__FILE__));
+
+//load all helper functions
+foreach(glob("$doc_root/app/helpers/*.php") as $file)
+	include_once $file;
  
 $config = array(
 	"admin_password" 		=> "",
@@ -20,7 +23,7 @@ $saved = 0;
  
 if(sizeof($_POST) > 0) {
 	foreach($config as $key => $val) {
-		if(trim($_POST[$key]) !== "") {
+		if(isset($_POST[$key]) && trim($_POST[$key]) !== "") {
 			switch($key) {
 				case "host":
 					$config[$key] = strtolower($_POST[$key]);
@@ -37,7 +40,7 @@ if(sizeof($_POST) > 0) {
 		}
 	}
 	
-	//$saved = @file_put_contents("$doc_root/app/config/config.php", "<?php\n\n".'$config = '.var_export($new_config, true).";");
+	$saved = @file_put_contents("$doc_root/app/config/config.php", "<?php\n\n".'$config = '.var_export($config, true).";");
 	
 	if($saved)
 		setcookie("lando_password", $config["admin_password"], 0, "/", ".".$_SERVER['HTTP_HOST']);
@@ -46,5 +49,6 @@ if(sizeof($_POST) > 0) {
 //redirect to auth page for Dropbox
 //redirect back
 //store keys in app/config/<host_name>.php
-//if install_content checked, install content
+//if install_content checked, copy across content with progress
+//do initial cache of everything with progress
 //notify install complete and suggest delete install folder
