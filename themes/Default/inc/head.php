66252cc91d267b6c0e938cf028bc4dada773bbdb
Sam Rayner <samrayner@gmail.com>
2012-01-19 19:06:54 +0000

    Improve templates and CSS with HTML5 Boilerplate

--- themes/Default/inc/head.php
+++ themes/Default/inc/head.php
@@ -3,6 +3,8 @@
 
 <head>
 	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<meta name="description" content="<?php echo $site_description ?>" />
 	
 	<?php if(path_segment(1) == "drafts") echo '<meta name="robots" content="noindex, nofollow" />' ?>
@@ -16,11 +18,16 @@
 	<!-- Flux styles -->
 	<link rel="stylesheet" href="<?php echo $theme_dir ?>/css/flux.css" media="screen" />
 	
-	<!--[if lte IE 8]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<!--[if lte IE 8]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	
+	<!--[if lte IE 6]>
+		<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.2/CFInstall.min.js"></script>
+		<script>window.attachEvent("onload",function(){CFInstall.check({mode:"overlay"})})</script>
+	<![endif]-->
 	
   <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if necessary -->
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.js"></script>
-	<script>window.jQuery || document.write('<script src="<?php echo $theme_dir ?>/js/jquery-1.6.2.min.js">\x3C/script>')</script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.js"></script>
+	<script>window.jQuery || document.write('<script src="<?php echo $theme_dir ?>/js/jquery-1.7.1.min.js">\x3C/script>')</script>
 	
 	<!-- PrettyPhoto for galleries: http://www.no-margin-for-errors.com/projects/prettyphoto-jquery-lightbox-clone/ -->
 	<script src="<?php echo $theme_dir ?>/js/prettyPhoto/jquery.prettyPhoto.js"></script>
