ffeb99022541a7e7854446d188e5644b4cf9ceb3
Sam Rayner <sam@samrayner.com>
2012-10-08 15:53:23 +0100

    Recognise linebreaks properly when parsing CSVs

--- app/parsers/CSV/CSV_Parser.php
+++ app/parsers/CSV/CSV_Parser.php
@@ -4,7 +4,7 @@ class CSV_Parser {
 	public function parse($str) {
 		$html = "<table>\n\t<thead>\n";
 		
-		foreach(str_getcsv($str, "\n") as $i => $row) {			
+		foreach(str_getcsv($str, "\r\n") as $i => $row) {			
 			$html .= "\t\t<tr>";
 			
 			$cell_tag = ($i == 0) ? "th" : "td";
